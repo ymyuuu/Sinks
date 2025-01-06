e31cd57b0177e5039ac99130e77613fe7fc15604
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2025-01-06 14:35:21 +0800

    Update index.vue

--- components/login/index.vue
+++ components/login/index.vue
@@ -4,7 +4,7 @@ import { toast } from 'vue-sonner'
 import { z } from 'zod'
 
 const LoginSchema = z.object({
-  token: z.string().describe('SiteToken'),
+  token: z.string().describe('站点令牌'),
 })
 const loginFieldConfig = {
   token: {
@@ -25,7 +25,7 @@ async function onSubmit(form) {
   }
   catch (e) {
     console.error(e)
-    toast.error('Login failed, please try again.', {
+    toast.error('登录失败，请重试。', {
       description: e.message,
     })
   }
@@ -36,10 +36,10 @@ async function onSubmit(form) {
   <Card class="w-full max-w-sm">
     <CardHeader>
       <CardTitle class="text-2xl">
-        Login
+        登录
       </CardTitle>
       <CardDescription>
-        Enter your site token to login.
+        输入您的站点令牌以登录。
       </CardDescription>
     </CardHeader>
     <CardContent class="grid gap-4">
@@ -51,13 +51,13 @@ async function onSubmit(form) {
       >
         <Alert v-if="previewMode">
           <AlertCircle class="w-4 h-4" />
-          <AlertTitle>Tips</AlertTitle>
+          <AlertTitle>提示</AlertTitle>
           <AlertDescription>
-            The site token for preview mode is <code class="font-mono text-green-500">SinkCool</code> .
+            预览模式下的站点令牌是 <code class="font-mono text-green-500">SinkCool</code> 。
           </AlertDescription>
         </Alert>
         <Button class="w-full">
-          Login
+          登录
         </Button>
       </AutoForm>
     </CardContent>
