88a78010ded0d3ae685dbd28854ac304b0cc27e5
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2025-01-07 08:28:03 +0800

    Update Index.vue

--- components/dashboard/metrics/Index.vue
+++ components/dashboard/metrics/Index.vue
@@ -3,23 +3,28 @@
     <LazyDashboardMetricsLocations class="col-span-1 lg:col-span-8" />
     <DashboardMetricsGroup
       class="lg:col-span-4"
-      :tabs="['国家', '地区', '城市']"
+      :tabs="['country', 'region', 'city']"
+      :labels="{ country: '国家', region: '地区', city: '城市' }"
     />
     <DashboardMetricsGroup
       class="lg:col-span-6"
-      :tabs="['来源', '路径']"
+      :tabs="['referer', 'slug']"
+      :labels="{ referer: '来源', slug: '路径' }"
     />
     <DashboardMetricsGroup
       class="lg:col-span-6"
-      :tabs="['语言', '时区']"
+      :tabs="['language', 'timezone']"
+      :labels="{ language: '语言', timezone: '时区' }"
     />
     <DashboardMetricsGroup
       class="lg:col-span-6"
-      :tabs="['设备', '设备类型']"
+      :tabs="['device', 'deviceType']"
+      :labels="{ device: '设备', deviceType: '设备类型' }"
     />
     <DashboardMetricsGroup
       class="lg:col-span-6"
-      :tabs="['操作系统', '浏览器', '浏览器类型']"
+      :tabs="['os', 'browser', 'browserType']"
+      :labels="{ os: '操作系统', browser: '浏览器', browserType: '浏览器类型' }"
     />
   </main>
 </template>
