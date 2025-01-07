cf6002bff23ad26f496f4b513d214975e97204b9
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2025-01-07 08:31:05 +0800

    Update Group.vue

--- components/dashboard/metrics/Group.vue
+++ components/dashboard/metrics/Group.vue
@@ -6,15 +6,13 @@ defineProps({
     type: Array,
     required: true,
   },
-  labels: {
-    type: Object,
-    default: () => ({}),
-  },
 })
 
 function type2name(type) {
-  // 优先使用 labels 中定义的映射
-  return labels[type] || pluralize(type.replace(type[0], type[0].toUpperCase()))
+  if (['os'].includes(type))
+    return type.toUpperCase()
+
+  return pluralize(type.replace(type[0], type[0].toUpperCase()))
 }
 </script>
 
